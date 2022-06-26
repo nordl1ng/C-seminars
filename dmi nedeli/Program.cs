642b93e29c2ede46c83599bb005fe31824989965
DmitryZ <zagarskih@inbox.ru>
2022-06-26 11:31:07 +0500

    seminars

---
+++ dmi nedeli/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите цифру");
+int day = int.Parse(Console.ReadLine());
+Console.WriteLine("День недели ");
+switch (day)
+{
+    case 1: Console.WriteLine("Понедельник "); break;
+    case 2: Console.WriteLine("Вторник "); break;
+    case 3: Console.WriteLine("Среда "); break;
+    case 4: Console.WriteLine("Четверг "); break;
+    case 5: Console.WriteLine("Пятница "); break;
+    case 6: Console.WriteLine("Суббота "); break;
+    case 7: Console.WriteLine("Воскресенье "); break;
+    default: Console.WriteLine("Введите от 1 до 7 "); break;
+}
+if (a %2 =0)
---
+++ proverka kvadrata/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numberB = int.Parse(Console.ReadLine()); ;
+if (numberA * numberA == numberB)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
